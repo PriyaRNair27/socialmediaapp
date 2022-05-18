d6d9839fd9d7c03b77b31a7e04424f9cde8778d1
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-18 13:37:27 +0530

    nav added

--- src/Components/Searchpost.js
+++ src/Components/Searchpost.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../Header'
 
 const Searchpost = () => {
   return (
     <div>
-
+ <Header/>
 
 <div className="container">
     <div className="row">
